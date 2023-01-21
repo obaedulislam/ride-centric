0679b2d7e7c1a20432bd3d52c46ccc1fdc05b784
obayedul islam <obaed.cse@gmail.com>
2023-01-21 23:51:49 +0600

    Why Us section end

---
+++ js/main.js
@@ -0,0 +1,37 @@
+$(".fleets-img").owlCarousel({
+  loop: true,
+  nav: true,
+  autoplay: true,
+  autoplayTimeout: 3000,
+  autoplayHoverPause: true,
+  responsive: {
+    0: {
+      items: 2,
+    },
+    600: {
+      items: 3,
+    },
+    1000: {
+      items: 3,
+    },
+  },
+});
+
+$(".person").owlCarousel({
+  loop: true,
+  nav: true,
+  autoplay: true,
+  autoplayTimeout: 3000,
+  autoplayHoverPause: true,
+  responsive: {
+    0: {
+      items: 1,
+    },
+    600: {
+      items: 1,
+    },
+    1000: {
+      items: 1,
+    },
+  },
+});
